c15024b19660742644fcec8d1110e66a2fc1c6c5
Matej Schwartz <sch.matej@gmail.com>
2018-09-12 09:49:30 +0200

    Added select recipe and show detail, Added adding ingredient to shopping list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -9,12 +9,17 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent implements OnInit {
 
   recipes: Recipe[] = [
-    new Recipe('Cheesecake', 'Per Serving: 313 calories; 17.7 g fat; 35.8 g carbohydrates; 4.5 g protein; 88 mg cholesterol; 272 mg sodium. ', 'https://images.media-allrecipes.com/userphotos/560x315/5520328.jpg')
+    new Recipe('Cheesecake', 'Per Serving: 313 calories; 17.7 g fat; 35.8 g carbohydrates; 4.5 g protein; 88 mg cholesterol; 272 mg sodium. ', 'https://images.media-allrecipes.com/userphotos/560x315/5520328.jpg'),
+    new Recipe('Chicken', 'It\'s the all-American meal. Find out how to make juicy fried chicken with a crispy golden crust every time.', 'https://images.media-allrecipes.com/userphotos/600x600/1585625.jpg')
   ];
 
+  @Output() recipeWasSelected: EventEmitter<Recipe> = new EventEmitter<Recipe>()
   constructor() { }
 
   ngOnInit() {
   }
 
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
