8951e978c8120bffc38362f959d78506bb7a301e
Matej Schwartz <sch.matej@gmail.com>
2018-09-13 15:22:28 +0200

    Added function to copy recipe ingredients to shopping list

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { ShoppingListService } from '../../shopping-list/shopping-list.service';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -11,8 +13,12 @@ export class RecipeDetailComponent implements OnInit {
   @Input()
   recipe: Recipe;
 
-  constructor() { }
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
+  }
+
+  onAddIngredientsToShoppingList() {
+    this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
   }
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,5 +1,7 @@
 import { Injectable, EventEmitter } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
 @Injectable({
   providedIn: 'root'
@@ -8,13 +10,27 @@ export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('Cheesecake', 'Per Serving: 313 calories; 17.7 g fat; 35.8 g carbohydrates; 4.5 g protein; 88 mg cholesterol; 272 mg sodium. ', 'https://images.media-allrecipes.com/userphotos/560x315/5520328.jpg'),
-    new Recipe('Chicken', 'It\'s the all-American meal. Find out how to make juicy fried chicken with a crispy golden crust every time.', 'https://images.media-allrecipes.com/userphotos/600x600/1585625.jpg')
+    new Recipe(
+      'Cheesecake',
+      'Per Serving: 313 calories; 17.7 g fat; 35.8 g carbohydrates; 4.5 g protein; 88 mg cholesterol; 272 mg sodium.',
+      'https://images.media-allrecipes.com/userphotos/560x315/5520328.jpg',
+      [new Ingredient('Sugar', 120), new Ingredient('Butter', 0.25)]
+    ),
+    new Recipe(
+      'Vanilla Cake',
+      'A vanilla cake recipe that is quick, easy, and delicious!',
+      'https://addapinch.com/wp-content/uploads/2017/02/best-vanilla-cake-recipe-0429-crop-copy-210x267.jpg',
+      [new Ingredient('Flour', 250), new Ingredient('Baking Soda', 1), new Ingredient('Eggs', 4)]
+    )
   ];
 
-  constructor() { }
+  constructor(private shoppingListService: ShoppingListService) { }
 
   getRecipes() {
     return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.shoppingListService.addIngredients(ingredients);
   }
 }
